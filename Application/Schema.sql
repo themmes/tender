90c1f7d05e1b362ab7fb30f276188823e9a1c332
Tom Hemmes <t.hemmes@geophy.com>
2020-08-08 12:04:07 +0200

    Iniate new experiment with tables

---
+++ Application/Schema.sql
@@ -0,0 +1,18 @@
+CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
+CREATE TABLE projects (
+    id UUID DEFAULT uuid_generate_v4() PRIMARY KEY NOT NULL,
+    title TEXT DEFAULT '' NOT NULL
+);
+CREATE TABLE products (
+    id UUID DEFAULT uuid_generate_v4() PRIMARY KEY NOT NULL,
+    title TEXT DEFAULT '' NOT NULL,
+    price REAL NOT NULL
+);
+CREATE TABLE project_products (
+    id UUID DEFAULT uuid_generate_v4() PRIMARY KEY NOT NULL,
+    project_id UUID NOT NULL,
+    product_id UUID NOT NULL,
+    quantity INT NOT NULL
+);
+ALTER TABLE project_products ADD CONSTRAINT project_products_ref_project_id FOREIGN KEY (project_id) REFERENCES projects (id) ON DELETE NO ACTION;
+ALTER TABLE project_products ADD CONSTRAINT project_products_ref_product_id FOREIGN KEY (product_id) REFERENCES products (id) ON DELETE NO ACTION;
